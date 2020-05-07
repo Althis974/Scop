e188468c1be62500cda61ddf123ce825786a8f7c
Althis974 (Romeo Lossy) <rlossy@student.42.fr>
2020-05-07 12:13:19 +0200

    Test grey event

--- Shaders/testvertex.glsl
+++ Shaders/testvertex.glsl
@@ -11,10 +11,15 @@ out	vec2		txt_coor;
 
 void	main()
 {
+    float	grey;
+
 	gl_Position = mvp * vec4(pos, 1.0f);
 	if (color == 0)
-		fragment_color_s = vec4(pos.y * 0.4f + 0.4f, pos.z * 0.1 + pos.y * 0.4f + 0.1f, 0.2f, 1.0f);
-	if (color == 1)
+    {
+        grey = (0.2125 + 0.7154 + 0.0721) / 3.0f;
+        fragment_color_s = vec4(grey, grey, grey, 1.0f);
+    }
+    if (color == 1)
 		fragment_color_s = vec4(pos * 0.4f + 0.4f, 1.0f);
 	fragment_color_f = fragment_color_s;
     txt_coor = vec2(pos.x * 2, pos.y * 2);
