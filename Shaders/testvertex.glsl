42fd18663333f9aac30cd5ca80c14f1dd9b51234
Althis974 (Romeo Lossy) <rlossy@student.42.fr>
2020-05-07 12:17:51 +0200

    Update testvertex.glsl

--- Shaders/testvertex.glsl
+++ Shaders/testvertex.glsl
@@ -11,13 +11,10 @@ out	vec2		txt_coor;
 
 void	main()
 {
-    float	grey;
-
 	gl_Position = mvp * vec4(pos, 1.0f);
 	if (color == 0)
     {
-        grey = (pos.y * 0.2125 + pos.z * 0.7154 + pos.y * 0.0721) / 3.0f;
-        fragment_color_s = vec4(grey, grey, grey, 1.0f);
+        fragment_color_s = vec4(pos.y * 0.2125 + 0.2125, pos.z * 0.7154 + pos.y * 0.0721 + 0.0721, 0.2f, 1.0f);
     }
     if (color == 1)
 		fragment_color_s = vec4(pos * 0.4f + 0.4f, 1.0f);
