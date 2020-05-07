d53891c29a4a598de2fbb5f9019ccd2b329eab9d
Althis974 (Romeo Lossy) <rlossy@student.42.fr>
2020-05-07 14:35:30 +0200

    Grey shade should be ok

--- Shaders/testfragment.glsl
+++ Shaders/testfragment.glsl
@@ -6,24 +6,16 @@ in vec2				txt_coor;
 
 uniform bool		smod;
 uniform bool		apply;
-uniform bool		gmod;
 uniform sampler2D	txt;
 
 out vec4			col;
 
 void	main()
 {
-	float	grey;
-
 	if (smod)
 		col = fragment_color_s;
 	else
 		col = fragment_color_f;
 	if (apply)
 		col = texture(txt, txt_coor);
-    if (gmod)
-    {
-        grey = (0.2125 * col.x + 0.7154 * col.y + 0.0721 * col.z) / 3.0f;
-        col = vec4(grey, grey, grey, 1.0f);
-    }
 }
